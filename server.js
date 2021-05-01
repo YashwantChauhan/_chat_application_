42346aceba8f0625b010177e0f6eaadf823d0dbb
YashwantChauhan <yashwantyash123456789@gmail.com>
2021-05-01 13:36:09 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 require('dotenv').config();
+const pug  = require('pug')
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
@@ -10,12 +11,13 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render('pug/index');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5000
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
