a22b94078b3b821a4fe0aaf2a4d00da1f679944b
YashwantChauhan <yashwantyash123456789@gmail.com>
2021-05-01 19:15:05 +0530

    Middleware config

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 'use strict';
+
+const SESSION_SECRET = 'yashwant';
+
 const pug  = require('pug')
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session')
+const passport = require('passport')
 
 const app = express();
 
@@ -10,13 +15,26 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret : SESSION_SECRET,
+  resave : true,
+  saveUninitialized : true,
+  cookie : {
+    secure : false
+  }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.set('view engine', 'pug');
 
 app.route('/').get((req, res) => {
-  res.render('pug/index');
+  res.render('pug/index', {
+    'title' : 'Hello', 
+    'message' : 'Please login'
+  });
 });
 
-const PORT = 5000
+const PORT = 5000 || process.env.PORT
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
