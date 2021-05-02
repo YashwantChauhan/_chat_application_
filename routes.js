a7651bcabbba3ab7097d0846453a73086b4f4628
YashwantChauhan <yashwantyash123456789@gmail.com>
2021-05-03 00:12:26 +0530

    Task 12

--- routes.js
+++ routes.js
@@ -1,71 +1,75 @@
 const passport = require('passport')
 const bcrypt = require('bcrypt')
 
-module.exports = function(app,myDataBase){
+module.exports = function (app, myDataBase) {
 
-    app.route('/register').post((req,res,next)=>{
-        myDataBase.findOne({ username: req.body.username }, (err,user)=>{
-          if(err) next(err);
-          else if( user ){
-            res.redirect('/');
-          }
-          else{
-    
-            const hash = bcrypt.hashSync( req.body.password , 12 );
-            myDataBase.insertOne({
-              username: req.body.username,
-              password: hash
-            }, (err,doc)=>{
-              if(err) {
-                res.redirect('/')
-              }
-              else{
-                next(null,doc.ops[0])
-              }
-            })
-          }
+    app.route('/register').post((req, res, next) => {
+        myDataBase.findOne({ username: req.body.username }, (err, user) => {
+            if (err) next(err);
+            else if (user) {
+                res.redirect('/');
+            }
+            else {
+
+                const hash = bcrypt.hashSync(req.body.password, 12);
+                myDataBase.insertOne({
+                    username: req.body.username,
+                    password: hash
+                }, (err, doc) => {
+                    if (err) {
+                        res.redirect('/')
+                    }
+                    else {
+                        next(null, doc.ops[0])
+                    }
+                })
+            }
         })
-      }, passport.authenticate('local', { failureRedirect: '/' }), (req,res)=>{
-         res.redirect('/profile'); 
-      }
-      );
-    
-    
-      app.route('/profile').get(ensureAuthenticated, (req,res)=>{
+    }, passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+        res.redirect('/profile');
+    }
+    );
+
+
+    app.route('/profile').get(ensureAuthenticated, (req, res) => {
         res.render('pug/profile', {
-          username: req.user.username
+            username: req.user.username
         });
-      })
-    
-     
-        
-      app.route('/').get((req,res)=>{
-        res.render('pug', {
-          title: 'Connected to Database',
-          message: 'Please login',
-          showLogin: true,
-          showRegistration: true,
-          showSocialAuth: true
-        })
-      })
-    
-      
-      app.use((req,res,next)=>{
-        res.status(404).type('text').send('Not Found')
-      })
+    })
 
-     
+
+
+    app.route('/').get((req, res) => {
+        res.render('pug', {
+            title: 'Connected to Database',
+            message: 'Please login',
+            showLogin: true,
+            showRegistration: true,
+            showSocialAuth: true
+        })
+    })
+
+
+    app.use((req, res, next) => {
+        res.status(404).type('text').send('Not Found')
+    })
+
+    app.route('/auth/github').get(passport.authenticate('github'));
+    app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+        res.redirect('/profile')
+    })
+
 
 }
 
 
-function ensureAuthenticated(req,res,next){
+function ensureAuthenticated(req, res, next) {
 
-    if( req.isAuthenticated() ){
-     return next();
+    if (req.isAuthenticated()) {
+        return next();
     }
-    else{
-      res.redirect('/')
+    else {
+        res.redirect('/')
     }
-    
-  }
+
+}
