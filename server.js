882e1a77838a0145d35fcfcd3c8e013d3ffaab00
YashwantChauhan <yashwantyash123456789@gmail.com>
2021-05-02 19:14:08 +0530

    Task 10

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ myDB( async client=>{
   const myDataBase = await client.db('myFirstDatabase').collection('users');
 
   app.route('/register').post((req,res,next)=>{
-    myDataBase.findOne({ username : req.body.username }, (err,user)=>{
+    myDataBase.findOne({ username: req.body.username }, (err,user)=>{
       if(err) next(err);
       else if( user ){
         res.redirect('/');
@@ -55,7 +55,7 @@ myDB( async client=>{
         })
       }
     })
-  }, passport.authenticate('local', { failureRedirect : '/' }), (req,res)=>{
+  }, passport.authenticate('local', { failureRedirect: '/' }), (req,res)=>{
      res.redirect('/profile'); 
   }
   );
