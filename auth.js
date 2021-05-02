fd84f3b32ec4a40ecfa86c27b1fc067af7bbb137
YashwantChauhan <yashwantyash123456789@gmail.com>
2021-05-03 00:13:45 +0530

    Task 12

--- auth.js
+++ auth.js
@@ -16,10 +16,7 @@ module.exports = function(app,myDataBase){
   })
 
 
-  app.route('/auth/github').get(passport.authenticate('github'));
-  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req,res)=>{
-      res.redirect('/profile')
-  })
+  
   passport.serializeUser((user,done)=>{
     done(null,user._id);
   })
