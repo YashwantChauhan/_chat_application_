ac5797582d20b9a2a22fc1f485e2d31a8a4ff400
YashwantChauhan <yashwantyash123456789@gmail.com>
2021-05-04 23:43:49 +0530

    Task 1

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const session = require('express-session')
 const passport = require('passport')
 
 const routes = require('./routes')
+
+const passportSocketIo = require('passport.socketio')
+const cookieParser = require('cookie-parser')
+
 const auth = require('./auth');
-
-
-
 
 const app = express();
 
@@ -26,12 +27,17 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
+app.use( session({
   secret: process.env.SESSION_SECRET,
   resave: true,
+  store: store,
   saveUninitialized: true,
   cookie: { secure: false }
 }));
+
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url : URI })
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -46,10 +52,20 @@ myDB( async client=>{
   auth(app,myDataBase);
 
  
+  io.use( 
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: 'express.sid',
+      secret: process.env.SESSION_SECRET,
+      store: store,
+      success: onAuthorizeSuccess,
+      fail: onAuthorizeFail
+    })
+  )
   let currentUsers = 0;
 
   io.on('connection', socket=>{
-    console.log('A user has connected')
+    console.log(`${socket.request.user.name} has connected`)
     currentUsers++;
     io.emit('user count', currentUsers);
 
@@ -72,7 +88,16 @@ myDB( async client=>{
 
 
 
+function onAuthorizeSuccess(data,accept){
+  console.log('Successfull connection to socket.io');
+  accept(null,true);
+}
 
+function onAuthorizeFail(data, message, error, accept){
+  if(error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null,false);
+}
 //-----------------------------------------------------------------------------------------------------------------------------
 
 
